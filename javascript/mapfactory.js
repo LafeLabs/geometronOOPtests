be5c8f4a34ed7dd3dbd1352857dbc0530237bcb8
Lafe Spietz <lafelabs@gmail.com>
2019-04-27 10:24:13 -0400

    removed all the underscores to improve readability

--- javascript/mapfactory.js
+++ javascript/mapfactory.js
@@ -1,39 +1,44 @@
 
 
 function MapBox(w,h,map,div) {
-    this._w = w;
-    this._h = h;
-    this._map = map; 
-    this._div = div; 
+    this.w = w;
+    this.h = h;
+    this.map = map; 
+    this.div = div; 
 
-    this._div.style.width = this._w + "px";
-    this._div.style.height = this._h + "px";
+    this.div.style.width = this.w + "px";
+    this.div.style.height = this.h + "px";
 
 
     this.draw = function(MapBox) {
-        MapBox._div.innerHTML = "";
-        for(var index = 0;index < MapBox._map._array.length;index++){
-            var newdiv = document.createElement("DIV");
-            newdiv.style.position = "absolute";
-            MapBox._div.appendChild(newdiv);
-            newdiv.style.left = (MapBox._map._array[index]._x*MapBox._w).toString() + "px";
-            newdiv.style.top  = (MapBox._map._array[index]._y*MapBox._w).toString() + "px";
-            newdiv.style.width  = (MapBox._map._array[index]._w*MapBox._w).toString() + "px";
-            newdiv.style.transform  = "rotate(" + (MapBox._map._array[index]._angle).toString() + "deg)";
-            if(MapBox._map._array[index]._type == "image") {
+        MapBox.div.innerHTML = "";
+        for(var index = 0;index < MapBox.map.array.length;index++){
+            var newa = document.createElement("A");
+            newa.style.position = "absolute";
+            MapBox.div.appendChild(newa);
+            newa.style.left = (MapBox.map.array[index].x*MapBox.w).toString() + "px";
+            newa.style.top  = (MapBox.map.array[index].y*MapBox.w).toString() + "px";
+            newa.style.width  = (MapBox.map.array[index].w*MapBox.w).toString() + "px";
+            newa.style.transform  = "rotate(" + (MapBox.map.array[index].angle).toString() + "deg)";
+            if(MapBox.map.array[index].type == "image") {
                 var newimg = document.createElement("IMG");
                 newimg.style.position = "absolute";
                 newimg.style.left = "0px";
                 newimg.style.top = "0px";
                 newimg.style.width = "100%";
-                newdiv.appendChild(newimg);
-                newimg.src = MapBox._map._array[index]._url;
+                
+                if(MapBox.map.array[index].href.length > 0){
+                    newa.href = MapBox.map.array[index].href;
+                }
+                newa.appendChild(newimg);
+                
+                newimg.src = MapBox.map.array[index].url;
                 newimg.onload = function(){
                     this.parentElement.style.height = this.height + "px";
                 }
             }
-            if(MapBox._map._array[index]._type == "text"){
-                newdiv.innerHTML = MapBox._map._array[index]._text;
+            if(MapBox.map.array[index].type == "text"){
+                newa.innerHTML = MapBox.map.array[index].text;
             }
         }
     }
@@ -41,21 +46,21 @@ function MapBox(w,h,map,div) {
 }
 
 function Map(name) {
-    this._name = name;
-    this._array = [];
+    this.name = name;
+    this.array = [];
 }
 
 
 function MapLink(x,y,w,h,angle,text,href,url,type) {
-    this._x = x;
-    this._y = y;
-    this._w = w;
-    this._h = h;
-    this._angle = angle;
-    this._text = text;
-    this._href = href;
-    this._url = url;
-    this._type = type;
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.angle = angle;
+    this.text = text;
+    this.href = href;
+    this.url = url;
+    this.type = type;
 
 }
 
